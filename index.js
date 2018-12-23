92b9a99a5a625b35bde4ac57d6f6afc86966f4f7
Denys Dovhan <denysd@wix.com>
2018-12-24 00:09:48 +0200

    Add express server

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-const transGPSAPI = require('./trans-gps');
-const transportCVAPI = require('./transport-cv');
+const express = require('express');
+const transGPS = require('./trans-gps');
+const transportCV = require('./transport-cv');
 
-transportCVAPI
-  .fetchToken()
-  .then(token =>
-    Promise.all([
-      transGPSAPI.fetchRoutes(),
-      transGPSAPI.fetchTrackers(),
-      transportCVAPI.fetchRoutes(token),
-      transportCVAPI.fetchTrackers(token)
-    ])
-  )
-  .then(
-    ([
-      transGPSRoutes,
-      transGPSTrackers,
-      transportCVRoutes,
-      transportCVTrackers
-    ]) => ({
-      routes: [...transGPSRoutes, ...transportCVRoutes],
-      trackers: [...transGPSTrackers, ...transportCVTrackers]
-    })
-  )
-  .then(JSON.stringify)
-  .then(console.log);
+const app = express();
 
-// Common route object:
-//
-// const route = {
-//   id: 12, // route id, tracker's routeId is referenced to this field
-//   name: '29', // public route name
-//   color: 'red', // route color (TODO: Generate unique color)
-//   description: 'Держуніверситет - вул. Ковальчука' // Optional description of the route
-// };
+function fetchGPS() {
+  return transportCV
+    .fetchToken()
+    .then(token =>
+      Promise.all([
+        transGPS.fetchRoutes(),
+        transGPS.fetchTrackers(),
+        transportCV.fetchRoutes(token),
+        transportCV.fetchTrackers(token)
+      ])
+    )
+    .then(
+      ([
+        transGPSRoutes,
+        transGPSTrackers,
+        transportCVRoutes,
+        transportCVTrackers
+      ]) => ({
+        routes: [...transGPSRoutes, ...transportCVRoutes],
+        trackers: [...transGPSTrackers, ...transportCVTrackers]
+      })
+    );
+}
 
-// Common tracker object:
-//
-// const tracker = {
-//   id: 123,
-//   latitude: 12.12312313,
-//   longitude: 23.23413413,
-//   direction: 282.1, // angle/orientation
-//   speed: 23.3,
-//   datetime: '2018-12-23T15:12:24', // datetime/gpstime
-//   routeId: 123
-// };
+app.get('*', (req, res) => {
+  fetchGPS()
+    .then(data => res.json(data))
+    .catch(err => {
+      console.error(err);
+      res.end(err.toString());
+    });
+});
+
+app.listen(3000, () => console.log('Listen!'));
