676ac07db15aa44b2a807b95e001c710c3a8a35b
Denys Dovhan <denysd@wix.com>
2019-01-02 00:39:01 +0200

    Implement DataLayer. Close #1

--- api/index.js
+++ api/index.js
@@ -1,87 +1,49 @@
 const express = require('express');
 const cors = require('cors');
-const transGPS = require('./trans-gps');
-const transportCV = require('./transport-cv');
+const DataLayer = require('./data-layer');
+
 const EventStream = require('./event-stream');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const dataLayer = new DataLayer();
+
 app.use(cors());
 
-function fetchRoutes() {
-  return transportCV
-    .fetchToken()
-    .then(token =>
-      Promise.all([transGPS.fetchRoutes(), transportCV.fetchRoutes(token)])
-    )
-    .then(([transGPSRoutes, transportCVRoutes]) => [
-      ...transGPSRoutes,
-      ...transportCVRoutes
-    ]);
-}
-
-function fetchTrackers() {
-  return transportCV
-    .fetchToken()
-    .then(token =>
-      Promise.all([transGPS.fetchTrackers(), transportCV.fetchTrackers(token)])
-    )
-    .then(([transGPSTrackers, transportCVTrackers]) => [
-      ...transGPSTrackers,
-      ...transportCVTrackers
-    ]);
-}
-
-app.get('/routes', (req, res) => {
-  return fetchRoutes()
-    .then(routes => res.json(routes))
-    .catch(err => {
-      console.error(err);
-      res.end(err.toString());
-    });
+app.get('/routes', async (req, res) => {
+  try {
+    const routes = await dataLayer.fetchRoutes();
+    res.json(routes);
+  } catch (error) {
+    console.log(error);
+    res.end(err.toString());
+  }
 });
 
-app.get('/trackers', (req, res) => {
-  return fetchTrackers()
-    .then(trackers => res.json(trackers))
-    .catch(err => {
-      console.error(err);
-      res.end(err.toString());
-    });
+app.get('/trackers', async (req, res) => {
+  try {
+    const trackers = await dataLayer.fetchTrackers();
+    res.json(trackers);
+  } catch (error) {
+    console.log(error);
+    res.end(err.toString());
+  }
 });
-
-function sendTrackers(stream) {
-  return fetchTrackers()
-    .then(trackers => {
-      stream.send(trackers);
-    })
-    .catch(err => {
-      console.log(err);
-      stream.send('error!');
-    });
-}
 
 app.get('/events', (req, res) => {
   const stream = new EventStream(res);
 
   stream.open();
 
-  sendTrackers(stream);
+  // Keep the reference to the function, so we can unsubscribe then
+  const sendTrackers = trackers => stream.send(trackers);
 
-  const intervalId = setInterval(() => sendTrackers(stream), 5000);
+  dataLayer.subscribe(sendTrackers);
 
-  req.on('finish', () => {
-    console.log('finished!');
-    clearInterval(intervalId);
-  });
-  req.on('close', () => {
-    console.log('closed!');
-    clearInterval(intervalId);
-  });
+  req.on('finish', () => dataLayer.unsubcribe(sendTrackers));
+  req.on('close', () => dataLayer.unsubcribe(sendTrackers));
 });
-
-app.use('/app', express.static('app-old'));
 
 app.listen(PORT, () => console.log(`Listen on ${PORT}!`));
