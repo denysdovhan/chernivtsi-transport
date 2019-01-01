8da1f363d4a5a62b31bec045b8cca32d5f9423c2
Denys Dovhan <denysd@wix.com>
2019-01-02 00:42:06 +0200

    Better styles for map. Better icons. Close #8 and #14

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,11 @@ import L from 'leaflet';
 import * as RL from 'react-leaflet';
 import EventStreamClient from './sse-client';
 import renderSVG from './svg';
+import MapBoxGLLayer from './MapboxGL';
 
-const api = 'http://localhost:3001';
-const tileLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const api = 'http://192.168.0.106:3001';
+const tileLayer =
+  'https://maps.tilehosting.com/c/3e872111-766b-403a-8b3e-82feeccc6995/styles/bright/style.json?key=Mu3jnev7PZ551DrAd6cY';
 
 const root = {
   position: [48.2916063, 25.9345009],
@@ -27,7 +29,7 @@ class App extends Component {
     const stream = new EventStreamClient(`${api}/events`);
 
     stream.receive((markers, event) => {
-      console.log('receive data:', event);
+      console.log('receive data:', markers);
       if (Array.isArray(markers)) {
         this.setState({ markers });
       }
@@ -38,10 +40,17 @@ class App extends Component {
     const { routes, markers } = this.state;
 
     return (
-      <RL.Map center={root.position} zoom={13} style={{ height: '100%' }}>
-        <RL.TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url={tileLayer}
+      <RL.Map
+        center={root.position}
+        zoom={13}
+        zoomControl={false}
+        style={{ height: '100%' }}
+        onViewportChange={console.log}
+      >
+        <MapBoxGLLayer
+          attribution={`<a href="https://www.maptiler.com/license/maps/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>`}
+          accessToken="not-needed"
+          style={tileLayer}
         />
         {markers.map(marker => {
           const route = routes.find(route => route.id === marker.routeId);
@@ -57,7 +66,7 @@ class App extends Component {
                   text: route ? route.name : 'A',
                   stroke: route ? route.color : 'gray'
                 }),
-                iconAnchor: [55, 40]
+                iconAnchor: [13, 19]
               })}
             >
               <RL.Popup>{JSON.stringify(marker, null, 2)}</RL.Popup>
