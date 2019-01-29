f515e7360eac9dc4988754d2c2d2e22b069748c1
Denys Dovhan <denysdovhan@gmail.com>
2019-01-29 12:59:24 +0000

    Deploy denysdovhan/chernivtsi-transport to github.com/denysdovhan/chernivtsi-transport.git:gh-pages

---
+++ precache-manifest.1482d6de32873db13e82b1e2dcd7f3d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "436c83a34bb347ffae81",
+    "url": "/chernivtsi-transport/static/css/main.8497096b.chunk.css"
+  },
+  {
+    "revision": "436c83a34bb347ffae81",
+    "url": "/chernivtsi-transport/static/js/main.436c83a3.chunk.js"
+  },
+  {
+    "revision": "f49e7fb36b6c275d706c",
+    "url": "/chernivtsi-transport/static/css/1.d1da8a7e.chunk.css"
+  },
+  {
+    "revision": "f49e7fb36b6c275d706c",
+    "url": "/chernivtsi-transport/static/js/1.f49e7fb3.chunk.js"
+  },
+  {
+    "revision": "ee850b7e16823d1b9b58",
+    "url": "/chernivtsi-transport/static/js/runtime~main.ee850b7e.js"
+  },
+  {
+    "revision": "b440d955fde4e2130733e933aada63df",
+    "url": "/chernivtsi-transport/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chernivtsi-transport/precache-manifest.b4f9a27f624b92c8405f314cd976d0f2.js"
+  "/chernivtsi-transport/precache-manifest.1482d6de32873db13e82b1e2dcd7f3d5.js"
 );
 
 workbox.clientsClaim();
